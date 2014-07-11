3fb1396f07aa47d566874778a8ffd633a9dd0d3e
gavin <gavin@afrispatial.co.za>
2014-07-11 10:21:03 +0200

    capture forms and database work

--- scripts/dwa_work.sql
+++ scripts/dwa_work.sql
@@ -471,3 +471,265 @@ drop table sg.other_boundaries_;
 SET search_path = sg, public;
 
 alter table established_boundary alter column wkb_geometry set data type geometry(MultiLinestring,4148)
+
+--adding table for final dams, removing extraneous dams
+CREATE TABLE public.dwa_dams_final
+(
+   dam_no character varying(10)
+) 
+;
+
+copy dwa_dams_final from '/data/KirchhoffSurveyors/dwa_dams_final';
+
+DELETE FROM dams_all_geo WHERE gid NOT IN (SELECT gid FROM dams_all_geo a JOIN dwa_dams_final b USING (dam_no) ORDER BY gid);
+
+--setting up default style for farm_portions
+
+copy layer_styles from '/tmp/layerstyle.csv' with (format csv,header);
+
+update layer_styles set f_table_catalog = 'dwaregister';
+
+INSERT INTO layer_styles (f_table_catalog,f_table_schema,f_table_name,f_geometry_column,stylename,styleqml,stylesld,useasdefault,description,owner,update_time) VALUES (
+'dwaregister','sg','farm_portion','geom','farm_portion',XMLPARSE (DOCUMENT '<!DOCTYPE qgis PUBLIC "http://mrcc.com/qgis.dtd" "SYSTEM">
+<qgis version="2.5.0-Master" minimumScale="-4.65661e-10" maximumScale="500000" simplifyDrawingHints="1" minLabelScale="0" maxLabelScale="1e+08" simplifyDrawingTol="1" simplifyMaxScale="1" hasScaleBasedVisibilityFlag="1" simplifyLocal="0" scaleBasedLabelVisibilityFlag="0">
+ <renderer-v2 symbollevels="0" type="singleSymbol">
+  <symbols>
+   <symbol alpha="1" type="fill" name="0">
+    <layer pass="0" class="SimpleFill" locked="0">
+     <prop k="border_width_map_unit_scale" v="0,0"/>
+     <prop k="border_width_unit" v="MM"/>
+     <prop k="color" v="172,142,191,255"/>
+     <prop k="color_border" v="0,0,0,255"/>
+     <prop k="joinstyle" v="bevel"/>
+     <prop k="offset" v="0,0"/>
+     <prop k="offset_map_unit_scale" v="0,0"/>
+     <prop k="offset_unit" v="MM"/>
+     <prop k="style" v="no"/>
+     <prop k="style_border" v="solid"/>
+     <prop k="width_border" v="0.26"/>
+    </layer>
+   </symbol>
+  </symbols>
+  <rotation/>
+  <sizescale scalemethod="area"/>
+ </renderer-v2>
+ <customproperties>
+  <property key="labeling" value="pal"/>
+  <property key="labeling/addDirectionSymbol" value="false"/>
+  <property key="labeling/angleOffset" value="0"/>
+  <property key="labeling/blendMode" value="0"/>
+  <property key="labeling/bufferBlendMode" value="0"/>
+  <property key="labeling/bufferColorA" value="255"/>
+  <property key="labeling/bufferColorB" value="255"/>
+  <property key="labeling/bufferColorG" value="255"/>
+  <property key="labeling/bufferColorR" value="255"/>
+  <property key="labeling/bufferDraw" value="false"/>
+  <property key="labeling/bufferJoinStyle" value="64"/>
+  <property key="labeling/bufferNoFill" value="false"/>
+  <property key="labeling/bufferSize" value="1"/>
+  <property key="labeling/bufferSizeInMapUnits" value="false"/>
+  <property key="labeling/bufferSizeMapUnitMaxScale" value="0"/>
+  <property key="labeling/bufferSizeMapUnitMinScale" value="0"/>
+  <property key="labeling/bufferTransp" value="0"/>
+  <property key="labeling/centroidInside" value="false"/>
+  <property key="labeling/centroidWhole" value="false"/>
+  <property key="labeling/decimals" value="3"/>
+  <property key="labeling/displayAll" value="false"/>
+  <property key="labeling/dist" value="0"/>
+  <property key="labeling/distInMapUnits" value="false"/>
+  <property key="labeling/distMapUnitMaxScale" value="0"/>
+  <property key="labeling/distMapUnitMinScale" value="0"/>
+  <property key="labeling/enabled" value="false"/>
+  <property key="labeling/fieldName" value=""/>
+  <property key="labeling/fontBold" value="true"/>
+  <property key="labeling/fontCapitals" value="0"/>
+  <property key="labeling/fontFamily" value="Ubuntu"/>
+  <property key="labeling/fontItalic" value="false"/>
+  <property key="labeling/fontLetterSpacing" value="0"/>
+  <property key="labeling/fontLimitPixelSize" value="false"/>
+  <property key="labeling/fontMaxPixelSize" value="10000"/>
+  <property key="labeling/fontMinPixelSize" value="3"/>
+  <property key="labeling/fontSize" value="11"/>
+  <property key="labeling/fontSizeInMapUnits" value="false"/>
+  <property key="labeling/fontSizeMapUnitMaxScale" value="0"/>
+  <property key="labeling/fontSizeMapUnitMinScale" value="0"/>
+  <property key="labeling/fontStrikeout" value="false"/>
+  <property key="labeling/fontUnderline" value="false"/>
+  <property key="labeling/fontWeight" value="63"/>
+  <property key="labeling/fontWordSpacing" value="0"/>
+  <property key="labeling/formatNumbers" value="false"/>
+  <property key="labeling/isExpression" value="true"/>
+  <property key="labeling/labelOffsetInMapUnits" value="true"/>
+  <property key="labeling/labelOffsetMapUnitMaxScale" value="0"/>
+  <property key="labeling/labelOffsetMapUnitMinScale" value="0"/>
+  <property key="labeling/labelPerPart" value="false"/>
+  <property key="labeling/leftDirectionSymbol" value="&lt;"/>
+  <property key="labeling/limitNumLabels" value="false"/>
+  <property key="labeling/maxCurvedCharAngleIn" value="20"/>
+  <property key="labeling/maxCurvedCharAngleOut" value="-20"/>
+  <property key="labeling/maxNumLabels" value="2000"/>
+  <property key="labeling/mergeLines" value="false"/>
+  <property key="labeling/minFeatureSize" value="0"/>
+  <property key="labeling/multilineAlign" value="0"/>
+  <property key="labeling/multilineHeight" value="1"/>
+  <property key="labeling/namedStyle" value="Medium"/>
+  <property key="labeling/obstacle" value="true"/>
+  <property key="labeling/placeDirectionSymbol" value="0"/>
+  <property key="labeling/placement" value="1"/>
+  <property key="labeling/placementFlags" value="0"/>
+  <property key="labeling/plussign" value="false"/>
+  <property key="labeling/preserveRotation" value="true"/>
+  <property key="labeling/previewBkgrdColor" value="#ffffff"/>
+  <property key="labeling/priority" value="5"/>
+  <property key="labeling/quadOffset" value="4"/>
+  <property key="labeling/repeatDistance" value="0"/>
+  <property key="labeling/repeatDistanceMapUnitMaxScale" value="0"/>
+  <property key="labeling/repeatDistanceMapUnitMinScale" value="0"/>
+  <property key="labeling/repeatDistanceUnit" value="1"/>
+  <property key="labeling/reverseDirectionSymbol" value="false"/>
+  <property key="labeling/rightDirectionSymbol" value=">"/>
+  <property key="labeling/scaleMax" value="10000000"/>
+  <property key="labeling/scaleMin" value="1"/>
+  <property key="labeling/scaleVisibility" value="false"/>
+  <property key="labeling/shadowBlendMode" value="6"/>
+  <property key="labeling/shadowColorB" value="0"/>
+  <property key="labeling/shadowColorG" value="0"/>
+  <property key="labeling/shadowColorR" value="0"/>
+  <property key="labeling/shadowDraw" value="false"/>
+  <property key="labeling/shadowOffsetAngle" value="135"/>
+  <property key="labeling/shadowOffsetDist" value="1"/>
+  <property key="labeling/shadowOffsetGlobal" value="true"/>
+  <property key="labeling/shadowOffsetMapUnitMaxScale" value="0"/>
+  <property key="labeling/shadowOffsetMapUnitMinScale" value="0"/>
+  <property key="labeling/shadowOffsetUnits" value="1"/>
+  <property key="labeling/shadowRadius" value="1.5"/>
+  <property key="labeling/shadowRadiusAlphaOnly" value="false"/>
+  <property key="labeling/shadowRadiusMapUnitMaxScale" value="0"/>
+  <property key="labeling/shadowRadiusMapUnitMinScale" value="0"/>
+  <property key="labeling/shadowRadiusUnits" value="1"/>
+  <property key="labeling/shadowScale" value="100"/>
+  <property key="labeling/shadowTransparency" value="30"/>
+  <property key="labeling/shadowUnder" value="0"/>
+  <property key="labeling/shapeBlendMode" value="0"/>
+  <property key="labeling/shapeBorderColorA" value="255"/>
+  <property key="labeling/shapeBorderColorB" value="128"/>
+  <property key="labeling/shapeBorderColorG" value="128"/>
+  <property key="labeling/shapeBorderColorR" value="128"/>
+  <property key="labeling/shapeBorderWidth" value="0"/>
+  <property key="labeling/shapeBorderWidthMapUnitMaxScale" value="0"/>
+  <property key="labeling/shapeBorderWidthMapUnitMinScale" value="0"/>
+  <property key="labeling/shapeBorderWidthUnits" value="1"/>
+  <property key="labeling/shapeDraw" value="false"/>
+  <property key="labeling/shapeFillColorA" value="255"/>
+  <property key="labeling/shapeFillColorB" value="255"/>
+  <property key="labeling/shapeFillColorG" value="255"/>
+  <property key="labeling/shapeFillColorR" value="255"/>
+  <property key="labeling/shapeJoinStyle" value="64"/>
+  <property key="labeling/shapeOffsetMapUnitMaxScale" value="0"/>
+  <property key="labeling/shapeOffsetMapUnitMinScale" value="0"/>
+  <property key="labeling/shapeOffsetUnits" value="1"/>
+  <property key="labeling/shapeOffsetX" value="0"/>
+  <property key="labeling/shapeOffsetY" value="0"/>
+  <property key="labeling/shapeRadiiMapUnitMaxScale" value="0"/>
+  <property key="labeling/shapeRadiiMapUnitMinScale" value="0"/>
+  <property key="labeling/shapeRadiiUnits" value="1"/>
+  <property key="labeling/shapeRadiiX" value="0"/>
+  <property key="labeling/shapeRadiiY" value="0"/>
+  <property key="labeling/shapeRotation" value="0"/>
+  <property key="labeling/shapeRotationType" value="0"/>
+  <property key="labeling/shapeSVGFile" value=""/>
+  <property key="labeling/shapeSizeMapUnitMaxScale" value="0"/>
+  <property key="labeling/shapeSizeMapUnitMinScale" value="0"/>
+  <property key="labeling/shapeSizeType" value="0"/>
+  <property key="labeling/shapeSizeUnits" value="1"/>
+  <property key="labeling/shapeSizeX" value="0"/>
+  <property key="labeling/shapeSizeY" value="0"/>
+  <property key="labeling/shapeTransparency" value="0"/>
+  <property key="labeling/shapeType" value="0"/>
+  <property key="labeling/textColorA" value="255"/>
+  <property key="labeling/textColorB" value="0"/>
+  <property key="labeling/textColorG" value="0"/>
+  <property key="labeling/textColorR" value="0"/>
+  <property key="labeling/textTransp" value="0"/>
+  <property key="labeling/upsidedownLabels" value="0"/>
+  <property key="labeling/wrapChar" value=""/>
+  <property key="labeling/xOffset" value="0"/>
+  <property key="labeling/yOffset" value="0"/>
+ </customproperties>
+ <blendMode>0</blendMode>
+ <featureBlendMode>0</featureBlendMode>
+ <layerTransparency>0</layerTransparency>
+ <displayfield>id</displayfield>
+ <label>0</label>
+ <labelattributes>
+  <label fieldname="" text="Label"/>
+  <family fieldname="" name="Ubuntu"/>
+  <size fieldname="" units="pt" value="12"/>
+  <bold fieldname="" on="0"/>
+  <italic fieldname="" on="0"/>
+  <underline fieldname="" on="0"/>
+  <strikeout fieldname="" on="0"/>
+  <color fieldname="" red="0" blue="0" green="0"/>
+  <x fieldname=""/>
+  <y fieldname=""/>
+  <offset x="0" y="0" units="pt" yfieldname="" xfieldname=""/>
+  <angle fieldname="" value="0" auto="0"/>
+  <alignment fieldname="" value="center"/>
+  <buffercolor fieldname="" red="255" blue="255" green="255"/>
+  <buffersize fieldname="" units="pt" value="1"/>
+  <bufferenabled fieldname="" on=""/>
+  <multilineenabled fieldname="" on=""/>
+  <selectedonly on=""/>
+ </labelattributes>
+ <edittypes>
+  <edittype labelontop="0" editable="1" name="gid"/>
+  <edittype labelontop="0" editable="1" name="__gid"/>
+  <edittype labelontop="0" editable="1" name="prcl_key"/>
+  <edittype labelontop="0" editable="1" name="prcl_type"/>
+  <edittype labelontop="0" editable="1" name="lstatus"/>
+  <edittype labelontop="0" editable="1" name="wstatus"/>
+  <edittype labelontop="0" editable="1" name="geom_area"/>
+  <edittype labelontop="0" editable="1" name="comments"/>
+  <edittype labelontop="0" editable="1" name="tag_x"/>
+  <edittype labelontop="0" editable="1" name="tag_y"/>
+  <edittype labelontop="0" editable="1" name="tag_value"/>
+  <edittype labelontop="0" editable="1" name="tag_size"/>
+  <edittype labelontop="0" editable="1" name="tag_angle"/>
+  <edittype labelontop="0" editable="1" name="tag_just"/>
+  <edittype labelontop="0" editable="1" name="id"/>
+  <edittype labelontop="0" editable="1" name="date_stamp"/>
+ </edittypes>
+ <editform></editform>
+ <editforminit></editforminit>
+ <featformsuppress>0</featformsuppress>
+ <annotationform></annotationform>
+ <editorlayout>generatedlayout</editorlayout>
+ <excludeAttributesWMS/>
+ <excludeAttributesWFS/>
+ <attributeactions/>
+</qgis>'),
+XMLPARSE (DOCUMENT '<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se">
+ <NamedLayer>
+  <se:Name>farm_portion</se:Name>
+  <UserStyle>
+   <se:Name>farm_portion</se:Name>
+   <se:FeatureTypeStyle>
+    <se:Rule>
+     <se:Name>Single symbol</se:Name>
+     <se:PolygonSymbolizer>
+      <se:Stroke>
+       <se:SvgParameter name="stroke">#000000</se:SvgParameter>
+       <se:SvgParameter name="stroke-width">0.26</se:SvgParameter>
+       <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+      </se:Stroke>
+     </se:PolygonSymbolizer>
+    </se:Rule>
+   </se:FeatureTypeStyle>
+  </UserStyle>
+ </NamedLayer>
+</StyledLayerDescriptor>'),'t','Sun Jul 6 16:23:38 2014','gavin','2014-07-06 14:23:38.9327'
+);
+
+SELECT f_table_catalog,f_table_schema,f_table_name,f_geometry_column,stylename,styleqml,stylesld,useasdefault,description,owner,update_time FROM layer_styles;
+--I saved the styles as default styles locally again after setting postgresql config xmloption to document from content, after restore didn't work 
+--in the end I backed up layer styles, restored it into the db as a temp file then transferred all the records across to layer_styles
